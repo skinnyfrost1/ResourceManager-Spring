8bfbdb0f5e07faa3e591c214b3e7a06d1171bd1e
skinnyfrost1 <47397827+skinnyfrost1@users.noreply.github.com>
2020-09-03 15:47:53 -0400

    first commit

---
+++ src/test/java/com/resource/demo/ResourceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resource.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
