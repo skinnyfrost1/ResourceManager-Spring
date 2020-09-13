fcb6035ad0d4017c5f12d57b62cceceacbf35759
a1a4a7 <40833301+a1a4a7@users.noreply.github.com>
2020-09-13 15:37:09 -0400

    newest
    //=================================================================
    //    POST GET - /columns
    //    GET/PUT/DELETE - /columns/1
    //=================================================================

--- src/main/java/com/itlize/resourcemanager/controller/user/ColumnsController.java
+++ src/main/java/com/itlize/resourcemanager/controller/user/ColumnsController.java
@@ -1,31 +1,37 @@
-package com.itlize.resourcemanager.controller.user;
+//package com.itlize.resourcemanager.controller.user;
+//
+//
+//import java.util.List;
+//
+//import com.itlize.resourcemanager.entity.Columns;
+//import com.itlize.resourcemanager.repository.ColumnRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//
+//public class ColumnsController<Columns> {
+//
+//
+//    private ColumnRepository columnRepository;
+//
+//    @Autowired
+//    public ColumnsController(ColumnRepository theColumnRepository) {
+//        this.columnRepository = theColumnRe pository;
+//    }
+//
+
+//=================================================================
+//    POST GET - /columns
+//    GET/PUT/DELETE - /columns/1
+//=================================================================
 
 
-import java.util.List;
 
-import com.itlize.resourcemanager.entity.Columns;
-import com.itlize.resourcemanager.repository.ColumnRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-
-public class ColumnsController<Columns> {
-
-
-    private ColumnRepository columnRepository;
-
-    @Autowired
-    public ColumnsController(ColumnRepository theColumnRepository) {
-        this.columnRepository = theColumnRepository;
-    }
-
-
-
-    @GetMapping("columnsall")
-//    @Override
-    public List<Columns> findAll() {
-        return (List<Columns>) columnRepository.findAll();
-    }
+//    @GetMapping("columnsall")
+////    @Override
+//    public List<Columns> findAll() {
+//        return (List<Columns>) columnRepository.findAll();
+//    }
 
 }
