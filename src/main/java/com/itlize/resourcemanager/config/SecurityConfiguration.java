8e4ec7f8535664f76d8b1bc07388d7741f53f5ad
skinnyfrost1 <47397827+skinnyfrost1@users.noreply.github.com>
2020-09-14 11:51:16 -0400

    testing route security config.

--- src/main/java/com/itlize/resourcemanager/config/SecurityConfiguration.java
+++ src/main/java/com/itlize/resourcemanager/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.itlize.resourcemanager.config;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,6 +9,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .and()
+                .withUser("foo")
+                .password("foo")
+                .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("ADMIN","USER")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 }
